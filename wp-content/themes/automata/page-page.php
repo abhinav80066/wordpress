3380b285867b9ad025ab71732465d5d4d0988049
abhinav80066 <abhinav.chaudhary800@gmail.com>
2020-03-01 15:59:17 +0530

    third

---
+++ wp-content/themes/automata/page-page.php
@@ -0,0 +1,8 @@
+<?php  the_title(); ?>
+ <?php
+    // TO SHOW THE PAGE CONTENTS
+    while ( have_posts() ) : the_post(); ?> <!--Because the_content() works only inside a WP Loop -->
+        <div class="entry-content-page">
+            <?php the_content(); ?> <!-- Page Content -->
+        </div><!-- .entry-content-page -->
+       <?php endwhile; ?>
--- wp-content/themes/automata/page.php
+++ wp-content/themes/automata/page.php
@@ -1,51 +1,61 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <title> ABHIINAV CHAUDHARY </title>
-    <style type="text/css">
-        <h1 font-family:Comic Sans MS";>
-    </style>
-</head>
-<body>
-<header>
-    <ol>
-        <li><a href="#">HOME</a></li>
-        <li><a href="contact.html">CONTACT</a></li>
-        <li><a href="about.html">ABOUT</a></li>
-    </ol>
-</header>
-<div class="first">
-    <img src="C2.jpg" width="300px">
-    <h1>ABHINAV CHAUDHARY</h1>
-    <h3>wep developer/programmer/explorer</h3>
-</div>
-<hr>
-<div class=""second">
-<h2>INTRODUCTION</h2>
-<p>hello everyone myself Abhinav chaudhary and I am a computer science student .I started web development so my first project is to design my portfolio .I will add more information in this section soon as nowdays I don't get enough time soo till then enjoy your life as you only got one.</p>
+<!doctype html>
+<html lang="en">
+  <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-</div>
-<hr>
-<div class=""third">
-<h2>ABOUT MYSELF</h2>
-<p>when it is about myself I don't Know what to write but as this is only a project I write anything just don't judge me on that ,I will change that section when the project is comleted.I am a very shy person a and a bit unsocial as well.I am introvert type of person, I am very good when I am talkling with my friends but I don't know how to start to talk with a random person .I am a very joyfull person and always looking on the bright side of life and I hated the drama most people doing nowdays and that's also one of the reason of my unsocial and introvert side.just be happy in your life ,crete your own when you don't get one. </p>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-</div>
-<hr>
-<div class="fourth">
-    <h2>MY HOBBIES </h2>
-    <p>I Love to play football and volleyball,singing is also one of my hobbies. I also like to travel in the places with mountains and lots of trees, I just love the nature.you will amaze to know that programming is also one of my hobbies.</p>
+        <title><?php  bloginfo( 'name' ); ?> - <?php echo get_the_title(); ?></title>
+        <style type="text/css">
+            .logo {
+                height: 100px;
+            }
+        </style>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">
+                <?php
+                $custom_logo_id = get_theme_mod( 'custom_logo' );
+                $logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+                if ( has_custom_logo() ) { ?>
+                        <img src="<?php echo esc_url( $logo[0] ); ?>" class="logo">
+                        <?php 
+                } else {
+                        echo '<h1>'. get_bloginfo( 'name' ) .'</h1>';
+                } ?>
+                </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-</div>
-<hr>
-<div class="fifth">
-    <h2> MY PROJECTS </h2>
-    <ol>
-        <li><a href="fblogin.html"> A FACEBOOK LOGIN PAGE </a></li>
-        <li><a href="website.html">A SIMPLE WEBPAGE </a></li>
-        <li><a href="#"> PORTFOLIO </a></li>
-    </ol>
-</div>
-</body>
+            <?php wp_nav_menu( array( 'theme_location' => 'menu-1' ) ); ?>
+        </nav>
+        <?php while ( have_posts() ) {
+                the_post(); ?>
+                   <img src="<?php echo get_the_post_thumbnail_url(); ?>" class="w-50">
+                
+                <?php the_content();
+            } ?>
+        
+
+        <div class="container">
+            <?php while ( have_posts() ) {
+                the_post();
+
+                the_content();
+            } ?>
+        </div>
+
+
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
 </html>
